1737c40c2c7768b21b029ad6fbb4a3fa8e390ac3
kian0604 <m.kgrefiel06@gmail.com>
2025-10-11 11:51:33 +0800

    Delete restful-todoapp-backend directory

--- restful-todoapp-backend/src/main/java/com/example/todoappbackend/repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.todoappbackend.repository;
-
-import com.example.todoappbackend.model.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
