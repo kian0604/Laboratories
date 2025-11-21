6928a2932e156c4a9222196f0013410696018efa
kian0604 <m.kgrefiel06@gmail.com>
2025-11-21 13:15:38 +0800

    Add files via upload

---
+++ WS1013A_GREFIEL/lab-7/src/main/java/com/grefiel/lab_7/Product.java
@@ -0,0 +1,14 @@
+package com.grefiel.lab_7;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private Long id;
+    private String name;
+    private Double price;
+}
