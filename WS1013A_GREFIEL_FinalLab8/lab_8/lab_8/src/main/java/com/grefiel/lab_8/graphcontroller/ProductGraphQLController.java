30510eff064e626717abe5999312b1f2d5654aa4
kian0604 <m.kgrefiel06@gmail.com>
2025-11-28 14:56:34 +0800

    Add files via upload

---
+++ WS1013A_GREFIEL_FinalLab8/lab_8/lab_8/src/main/java/com/grefiel/lab_8/graphcontroller/ProductGraphQLController.java
@@ -0,0 +1,63 @@
+package com.grefiel.lab_8.graphcontroller;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+public class ProductGraphQLController {
+}
+package com.grefiel.lab_7.graphcontroller;
+
+import com.grefiel.lab_7.Product;
+import com.grefiel.lab_7.ProductService;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class ProductGraphQLController {
+
+    private final ProductService service;
+
+    public ProductGraphQLController(ProductService service) {
+        this.service = service;
+    }
+
+    // QUERIES ------------------------------
+
+    @QueryMapping
+    public List<Product> getAllProducts() {
+        return service.getAllProducts();
+    }
+
+    @QueryMapping
+    public Product getProductById(@Argument Long id) {
+        return service.getProductById(id);
+    }
+
+    // MUTATIONS ----------------------------
+
+    @MutationMapping
+    public Product addProduct(@Argument ProductInput product) {
+        Product p = new Product(null, product.getName(), product.getPrice());
+        return service.addProduct(p);
+    }
+
+    @MutationMapping
+    public Product updateProduct(@Argument Long id, @Argument ProductInput product) {
+        Product updated = new Product(id, product.getName(), product.getPrice());
+        return service.updateProduct(id, updated);
+    }
+
+    @MutationMapping
+    public Boolean deleteProduct(@Argument Long id) {
+        return service.deleteProduct(id);
+    }
+}
---
+++ WS1013A_GREFIEL_FinalLab8/lab_8/lab_8/src/main/java/com/grefiel/lab_8/graphcontroller/ProductInput.java
@@ -0,0 +1,9 @@
+package com.grefiel.lab_8.graphcontroller;
+
+import lombok.Data;
+
+@Data
+public class ProductInput {
+    private String name;
+    private Double price;
+}
